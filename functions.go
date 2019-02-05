91a309035f712741f8e20cb7910b5afc6ba6378a
Richard Jackson <rich@richlj.com>
2019-02-05 12:02:51 +0000

    adding listDirContents function

--- functions.go
+++ functions.go
@@ -1,6 +1,7 @@
 package pass
 
 import (
+	"io/ioutil"
 	"os/user"
 	"path/filepath"
 )
@@ -18,4 +19,23 @@ func getDirectoryPath() (*string, error) {
 	}
 	dir := filepath.Join(usr.HomeDir, mainDirectory)
 	return &dir, nil
+}
+
+// listDirContents returns a map of the names of the immediate contents of a
+// directory, and whether or not that item is itself a directory
+func listDirContents(dir string) (*map[string]bool, error) {
+	result := make(map[string]bool)
+	contents, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, item := range contents {
+		path := filepath.Join(dir, item.Name())
+		if item.IsDir() {
+			result[path] = true
+		} else {
+			result[path] = false
+		}
+	}
+	return &result, nil
 }
